75f819325ff9e76cb9fe65de5854c744c4276de5
YASSINE <contact@yassine.fun>
2024-03-26 18:28:44 -0700

    Rotate right

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -3,7 +3,7 @@
 /**
  * binary_tree_rotate_right - Performs a right-rotation on a binary tree
  * @tree: Pointer to the root node of the tree to rotate
- * 
+ *
  * Return: A pointer to the new root node of the tree once rotated
  */
 binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
