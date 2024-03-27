e315faaee2f93ae763fcb5f15d984c3fe22f9431
YASSINE <contact@yassine.fun>
2024-03-27 16:44:16 -0700

    Heap - Sort

--- binary_trees.h
+++ binary_trees.h
@@ -78,5 +78,6 @@ binary_tree_t *find_insert_pos(heap_t **root);
 heap_t *heap_insert(heap_t **root, int value);
 heap_t *array_to_heap(int *array, size_t size);
 int heap_extract(heap_t **root);
+int *heap_to_sorted_array(heap_t *heap, size_t *size);
 
 #endif
